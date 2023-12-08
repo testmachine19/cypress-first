2085bfc473c080b56887edd5dc9ae7c35dfeb8bd
testmachine19 <patysson@mail.ru>
2023-12-08 19:03:35 +0300

    Домашнее задание 5.1.2-verifier

---
+++ cypress/e2e/3-verifier/openpage.cy.js
@@ -0,0 +1,11 @@
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit(
+      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc"
+    );
+  });
+
+  it("can check an item on a page", () => {
+    cy.get("Home").should("be.visible");
+  });
+});
